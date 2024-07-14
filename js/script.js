20b8cfb1ea10f57e22a02879213e0b735e823e7e
1rominafrenquelli <rominaf462@gmail.com>
2024-07-14 19:46:57 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let header = `
+<h3>Romina Frenquelli</h3>
+<nav>
+    <ul>
+        <li><a href="#Sobre mi">Sobre mi</a></li>
+        <li><a href="#Mis Aptitudes">Mis Aptitudes</a></li>
+        <li><a href="#Cursos Realizados">Cursos Realizados</a></li>
+        <li><a href="#Mis Proyectos">Mis Proyectos</a></li>
+        <li><a href="#Contacto">Contacto</a></li>
+    </ul>
+</nav>`
+
+document.getElementById("idheader").innerHTML = header
+
+let footer = `
+<a href="https://github.com/RominaFrenquelli?tab=repositories/" target="_blank"><img src="img/github.png" alt="github"></a>
+<a href="https://ar.linkedin.com/" target="_blank"><img src="img/R.png" alt="linkedIn"></a>
+`
+
+document.getElementById("idfooter").innerHTML = footer
