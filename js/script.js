fb633cb29544e092db182c2769ece2ca980ba5c4
1rominafrenquelli <rominaf462@gmail.com>
2025-03-01 12:09:01 -0300

    actual

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-let header = `
-<h3>Romina Frenquelli</h3>
-<nav>
-    <ul>
-        <li><a href="#Sobre mi">Sobre mi</a></li>
-        <li><a href="#Mis Aptitudes">Mis Aptitudes</a></li>
-        <li><a href="#Cursos Realizados">Cursos Realizados</a></li>
-        <li><a href="#Mis Proyectos">Mis Proyectos</a></li>
-        <li><a href="#Contacto">Contacto</a></li>
-    </ul>
-</nav>`
 
-document.getElementById("idheader").innerHTML = header
+// let footer = `
+// <a href="https://github.com/RominaFrenquelli?tab=repositories/" target="_blank"><img src="img/github.png" alt="github"></a>
+// <a href="https://ar.linkedin.com/" target="_blank"><img src="img/R.png" alt="linkedIn"></a>
+// `
 
-let footer = `
-<a href="https://github.com/RominaFrenquelli?tab=repositories/" target="_blank"><img src="img/github.png" alt="github"></a>
-<a href="https://ar.linkedin.com/" target="_blank"><img src="img/R.png" alt="linkedIn"></a>
-`
-
-document.getElementById("idfooter").innerHTML = footer
+// document.getElementById("idfooter").innerHTML = footer
